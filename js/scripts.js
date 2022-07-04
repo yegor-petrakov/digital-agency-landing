0a891274155a9b43e12a3da62bb8a3e7f08eb7d3
yegor-petrakov <egor.petrakov.xyz@gmail.com>
2022-07-04 11:12:30 +0300

    update modal menu

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,12 @@ const menuBars = document.querySelector(".navbar__menu-bars");
 const modalMenuContainer = document.querySelector(".modal-menu__container");
 
 menuBars.addEventListener('click', () => {
-    if (modalMenuContainer.classList[1] != "active") {
+    if (modalMenuContainer.classList.contains("active") == false) {
         modalMenuContainer.classList.add("active");
+        menuBars.classList.add("clicked")
     } else {
         modalMenuContainer.classList.remove("active");
+        menuBars.classList.remove("clicked")
     }
     
 });
